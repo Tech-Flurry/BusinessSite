1661066047b881f571b1f4b60093aacef3d27d6e
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2022-08-17 14:55:01 +0500

    added blog page

--- Techflurry.BusinessSite.Models/BlogModel.cs
+++ Techflurry.BusinessSite.Models/BlogModel.cs
@@ -4,37 +4,30 @@
     {
         public BlogModel()
         {
-            Paragraphs = new List<Paragraph>();
             Comments = new List<Comment>();
             Tags = new List<string>();
         }
+        public AuthorModel Author { get; set; }
         public string Heading { get; set; }
+        public string MainImage { get; set; }
         public string ShortTitle { get; set; }
+        public string Identifier { get; set; }
         public DateTime DatePosted { get; set; }
         public string Category { get; set; }
-        public string BlogType { get; set; }
-        public List<Paragraph> Paragraphs { get; set; }
+        public BlogType BlogType { get; set; }
+        public string Content { get; set; }
         public List<string> Tags { get; set; }
-        public Author Author { get; set; }
         public List<Comment> Comments { get; set; }
 
     }
-    public class Paragraph
-    {
-        public string Content { get; set; }
-        public string Type { get; set; }
-    }
-    public class Author
-    {
-        public string Name { get; set; }
-        public string Picture { get; set; }
-        public string Bio { get; set; }
-    }
     public class Comment
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public UserModel User { get; set; }
         public string Content { get; set; }
         public DateTime PostedTime { get; set; }
+    }
+    public enum BlogType
+    {
+        SinglePage
     }
 }
---
+++ Techflurry.BusinessSite.Models/UserModel.cs
@@ -0,0 +1,35 @@
+﻿namespace TechFlurry.BusinessSite.Models
+{
+    public class UserModel : DocumentModel
+    {
+        public UserModel()
+        {
+            InitValues();
+        }
+        public UserType UserType { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+
+        protected virtual void InitValues()
+        {
+            UserType = UserType.Basic;
+        }
+    }
+    public class AuthorModel : UserModel
+    {
+        public string Bio { get; set; }
+        public string NickName { get; set; }
+
+        protected override void InitValues()
+        {
+            UserType = UserType.Author;
+        }
+    }
+    public enum UserType
+    {
+        Basic,
+        Author
+    }
+}
